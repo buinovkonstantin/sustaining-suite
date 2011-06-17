1c3b9c3f736909ceea5c647c813433f8f875adc8
Buinov.Konstantin@gmail.com <Buinov.Konstantin@gmail.com@ff89aacc-af14-d3f3-02ae-7ccaac16e280>
2011-06-17 04:42:53 +0000

    EasyMock and JUnit are added

---
+++ SustainingSuite/junit/common/params/ParamsImplTestCase.java
@@ -0,0 +1,67 @@
+package common.params;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.w3c.dom.Node;
+
+public class ParamsImplTestCase {
+
+	@Test
+	public void testParamsImplNode() {
+		Node node = EasyMock.createNiceMock(Node.class);
+		new ParamsImpl(node);
+	}
+
+	@Test
+	public void testParamsImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParamsImplParams() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutParams() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllParamsWithName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXmlContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
--- SustainingSuite/src/common/params/ParamsImpl.java
+++ SustainingSuite/src/common/params/ParamsImpl.java
@@ -19,45 +19,97 @@ import org.w3c.dom.NodeList;
 
 import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 
+/**
+ * Implementation of XML-wrapping class. 
+ * Intended for usage as parameters bucket.
+ * 
+ * @author buinok
+ */
 public class ParamsImpl implements Params {
 	
-	private static final String PARAMETERS_ROOT = "parameters";
+	// default name of root tag in wrapped XML document
+	private static final String PARAMETERS_ROOT_DEFAULT = "parameters";
+	
+	// tag name holding simple [name:value] pair
 	private static final String PARAMETER = "parameter";
 	private static final String PARAMETER_NAME = "name";
 	private static final String PARAMETER_VALUE = "value";
 	
+	// XML document owning child tags which are wrapped by this class 
 	private Document document;
+	// root element of XML document
 	private Element root;
 
+	/**
+	 * Constructor creates new parameters bucket where
+	 * its content is duplicated from XML node's content
+	 * @param node - source of content for parameters bucket
+	 */
 	public ParamsImpl(Node node) {
 		document = new DocumentImpl();
-		document.appendChild(document.importNode(node, true));
-		root = document.getDocumentElement();
+		if(node == null) {
+			root = document.createElement(PARAMETERS_ROOT_DEFAULT);
+			document.appendChild(root);
+		} else {
+			document.appendChild(document.importNode(node, true));
+			root = document.getDocumentElement();
+		}
 	}
 
+	/**
+	 * Default constructor. Creates empty parameters bucket
+	 * with default root tag name
+	 */
 	public ParamsImpl() {
 		document = new DocumentImpl();
-		root = document.createElement(PARAMETERS_ROOT);
+		root = document.createElement(PARAMETERS_ROOT_DEFAULT);
 		document.appendChild(root);
 	}
 
+	/**
+	 * De-facto copy constructor. Creates new parameters bucket
+	 * then fill it by duplicated content of @param params argument 
+	 */
 	public ParamsImpl(Params params) {
 		document = new DocumentImpl();
-		document.appendChild(document.importNode(params.getXmlContent(),true));
-		root = document.getDocumentElement();
+		if(params == null) {
+			root = document.createElement(PARAMETERS_ROOT_DEFAULT);
+			document.appendChild(root);
+		} else {
+			document.appendChild(document.importNode(params.getXmlContent(),true));
+			root = document.getDocumentElement();
+		}
 	}
 
+	/**
+	 * Sets name of root tag to @param name if it's not null,
+	 * otherwise just ignores call
+	 */
 	@Override
 	public void setName(String name) {
-		document.renameNode(root, null, name);
+		if(name != null)
+			document.renameNode(root, null, name);
 	}
 	
+	/**
+	 * Tries to get String value of parameter with specified @param name
+	 * @return String value if it exists in parameters bucket
+	 * @throws ParamsException if there is a corruption in wrapped
+	 * 		XML document
+	 * @throws ParamsException if there are no parameters with specified @param name
+	 */
 	@Override
 	public String getString(String name) throws ParamsException {
+		if(name == null)
+			throw new ParamsException("Invalid parameter name ["+name+"]");
+		
+		// enumerate through all tags with endpoint parameters
 		NodeList list = root.getElementsByTagName(PARAMETER);
 		for(int index = 0; index < list.getLength(); index++) {
 			NamedNodeMap attributes = list.item(index).getAttributes();
+			// check only tags with existing attributes
 			if(attributes != null) {
+				// try to find name attribute
 				Node nameAttribute = attributes.getNamedItem(PARAMETER_NAME);
 				if(nameAttribute == null)
 					throw new ParamsException("Missing parameter name");
@@ -69,6 +121,7 @@ public class ParamsImpl implements Params {
 				if(!nameString.equals(name))
 					continue;
 					
+				// try to find value attribute
 				Node valueAttribute = attributes.getNamedItem(PARAMETER_VALUE);
 				if(valueAttribute == null)
 					throw new ParamsException("Missing parameter value");
@@ -84,8 +137,17 @@ public class ParamsImpl implements Params {
 		throw new ParamsException("There is no parameter ["+name+"]");
 	}
 
+	/**
+	 * Store string parameter into bucket
+	 * @param name - name to store parameter with
+	 * @param value - value of stored parameter
+	 */
 	@Override
 	public void putString(String name, String value) {
+		if(name == null || value == null)
+			throw new IllegalArgumentException("name ["+name+
+					"] and value ["+value+"] are mandatory");
+		
 		Element node = document.createElement(PARAMETER);
 		node.setAttribute(PARAMETER_NAME, name);
 		node.setAttribute(PARAMETER_VALUE, value);
@@ -93,8 +155,18 @@ public class ParamsImpl implements Params {
 		root.appendChild(node);
 	}
 
+	/**
+	 * Stores parameters bucket as a parameter for another bucket.
+	 * All nodes of stored bucket are duplicated
+	 * @param name - name to store parameter with
+	 * @param params - content of stored parameters bucket
+	 */
 	@Override
 	public void putParams(String name, Params params) {
+		if(name == null || params == null)
+			throw new IllegalArgumentException("name ["+name+
+					"] and parameter ["+params+"] are mandatory");
+		
 		Element node = document.createElement(name);
 		
 		Element xmlContent = params.getXmlContent();
@@ -107,8 +179,16 @@ public class ParamsImpl implements Params {
 		root.appendChild(node);
 	}
 	
+	/**
+	 * Returns all parameters buckets which are stored under actual one
+	 * @param - name of all parameters bucket to retrieve
+	 */
 	@Override
 	public Params[] getAllParamsWithName(String name) {
+		if(name == null)
+			throw new IllegalArgumentException("name ["+name+
+					"] is mandatory");
+		
 		NodeList list = root.getElementsByTagName(name);
 		
 		Params[] paramsArray = new Params[list.getLength()];
@@ -118,12 +198,18 @@ public class ParamsImpl implements Params {
 		
 		return paramsArray;
 	}
-	
+
+	/**
+	 * Return root wrapped XML tag 
+	 */
 	@Override
 	public final Element getXmlContent() {
 		return root;
 	}
-	
+
+	/**
+	 * Implements standard method to serialize hits object to String
+	 */
 	@Override
 	public String toString() {
 		TransformerFactory factory = TransformerFactory.newInstance();
@@ -143,51 +229,4 @@ public class ParamsImpl implements Params {
 		
 		return null;
 	}
-	
-    public static void main(String[] args) {
-		Document document = new DocumentImpl();
-		Element root = document.createElement("root");
-		document.appendChild(root);
-		
-		Element node = document.createElement(PARAMETER);
-		node.setAttribute(PARAMETER_NAME, "oldName");
-		node.setAttribute(PARAMETER_VALUE, "oldConnection");
-		
-		root.appendChild(node.cloneNode(true));
-		
-		node.setAttribute(PARAMETER_NAME, "newName");
-		node.setAttribute(PARAMETER_VALUE, "newConnection");
-		
-		root.appendChild(node.cloneNode(true));
-		
-		TransformerFactory factory = TransformerFactory.newInstance();
-		try {
-			Transformer transformer = factory.newTransformer();
-			Source source = new DOMSource(document);
-			StringWriter writer = new StringWriter();
-			Result result = new StreamResult(writer);
-			transformer.transform(source, result);
-			System.out.println(writer.toString());
-			
-			source = new DOMSource(root);
-			writer = new StringWriter();
-			result = new StreamResult(writer);
-			transformer.transform(source, result);
-			System.out.println(writer.toString());
-			
-			document.renameNode(root, null, PARAMETER);
-			source = new DOMSource(root);
-			writer = new StringWriter();
-			result = new StreamResult(writer);
-			transformer.transform(source, result);
-			System.out.println(writer.toString());
-
-		} catch (TransformerConfigurationException e) {
-			e.printStackTrace();
-		} catch (TransformerException e) {
-			e.printStackTrace();
-		}
-		
-
-    }
 }
