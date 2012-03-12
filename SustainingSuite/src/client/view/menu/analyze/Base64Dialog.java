2e85540afd303c7dc3143d1f356d9e04e21fd206
Buinov.Konstantin@gmail.com <Buinov.Konstantin@gmail.com@ff89aacc-af14-d3f3-02ae-7ccaac16e280>
2012-03-12 11:39:48 +0000

    Base64 encoding added

--- SustainingSuite/src/client/view/menu/analyze/Base64Dialog.java
+++ SustainingSuite/src/client/view/menu/analyze/Base64Dialog.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 import client.view.MainFrame;
 
@@ -19,6 +20,7 @@ import common.util.FileUtils;
 import javax.swing.*;
 
 import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 public class Base64Dialog extends JDialog {
 	
@@ -44,6 +46,7 @@ public class Base64Dialog extends JDialog {
 
     public Base64Dialog() {
         super(MainFrame.link, "Base64", false);
+        // decode section
         encodedOpenButton = new JButton("Open encoded file...");
         encodedOpenButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -74,10 +77,37 @@ public class Base64Dialog extends JDialog {
             }
         });
         
+        // encode section
         decodedOpenButton = new JButton("Open decoded file...");
+        decodedOpenButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	JFileChooser fileChooser = new JFileChooser();
+            	int retVal = fileChooser.showOpenDialog(Base64Dialog.this);
+            	if(retVal == JFileChooser.APPROVE_OPTION) {
+            		setDecodedFileAsSource(fileChooser.getSelectedFile());
+            	}
+            }
+        });
+        
         decodedArea = new JTextArea(10,20);
         encodeButton = new JButton("Encode");
+        encodeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+        		encodeToTextArea();
+            }
+        });
+        
         encodeAndSaveButton = new JButton("Encode and save as...");
+        encodeAndSaveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	JFileChooser fileChooser = new JFileChooser();
+            	int retVal = fileChooser.showOpenDialog(Base64Dialog.this);
+            	if(retVal == JFileChooser.APPROVE_OPTION) {
+            		encodeAndSaveAs(fileChooser.getSelectedFile());
+            	}
+            }
+        });
+
         FormLayout formLayout = new FormLayout("100dlu, 80dlu:grow(1), 90dlu", "20dlu, 20dlu, 80dlu:grow(.5), 20dlu, 20dlu, 20dlu, 20dlu, 80dlu:grow(.5), 20dlu");
         PanelBuilder panelBuilder = new PanelBuilder(formLayout);
         CellConstraints c = new CellConstraints();
@@ -190,6 +220,133 @@ public class Base64Dialog extends JDialog {
 			try {
 				outputStream = new FileOutputStream(selectedFile);
 				outputStream.write(decodedData);
+			} catch (FileNotFoundException fnfException) {
+				// TODO Auto-generated catch block
+				fnfException.printStackTrace();
+			} catch (IOException ioException) {
+				// TODO Auto-generated catch block
+				ioException.printStackTrace();
+			}
+		}
+	}
+	
+	private void setDecodedFileAsSource(File selectedFile) {
+		resetDecodedSource();
+		decodedFile = selectedFile;
+		
+		if(decodedFile.length() > MAXIMAL_AUTO_LOADED_FILE_SIZE) {
+			int choice = JOptionPane.showConfirmDialog(Base64Dialog.this, 
+					"The file may contain binary data, \n" +
+					"moreover its size exceeds " + 
+					MAXIMAL_AUTO_LOADED_FILE_SIZE + 
+					" bytes. \nDo you still want to get it loaded into edit area?");
+			if(choice == JOptionPane.NO_OPTION)
+				return;
+		} else {
+			int choice = JOptionPane.showConfirmDialog(Base64Dialog.this, 
+					"The file may contain binary data.\n" +
+					"Do you want to get it loaded into edit area?");
+			if(choice == JOptionPane.NO_OPTION)
+					return;
+		}
+		
+		FileInputStream inputStream;
+		try {
+			inputStream = new FileInputStream(decodedFile);
+			byte[] inputArray = new byte[100];
+			boolean stop = false;
+			while(!stop) {
+				int bytesRead = inputStream.read(inputArray);
+				if(bytesRead == -1) {
+					stop = true;
+					continue;
+				} else {
+					decodedArea.append(new String(inputArray, 0, bytesRead));
+					if(bytesRead < inputArray.length) {
+						stop = true;
+					}
+				}
+			}
+			
+			// drop association with original file to allow user 
+			// to modify loaded content for further decoding
+			decodedFile = null;
+		} catch (FileNotFoundException fnfException) {
+			// TODO Auto-generated catch block
+			fnfException.printStackTrace();
+		}
+		catch (IOException ioException) {
+			// TODO Auto-generated catch block
+			ioException.printStackTrace();
+		}
+	}
+
+	private void encodeToTextArea() {
+		resetEncodedSource();
+
+		String encodedData = encodeData();
+		if(encodedData == null)
+			JOptionPane.showMessageDialog(this, "Entered data is not valid Base64-encoded data", "Invalid source data", JOptionPane.ERROR_MESSAGE);
+		else
+			encodedArea.setText(encodedData);
+	}
+
+	private String encodeData() {
+		
+		if((decodedFile == null) || (decodedArea.getText().length() > 0)) {
+			return new BASE64Encoder().encodeBuffer(decodedArea.getText().getBytes());
+		} else {
+			FileInputStream inputStream;
+			StringBuilder encodedData = new StringBuilder();
+			BASE64Encoder encoder = new BASE64Encoder();
+			
+			try {
+				inputStream = new FileInputStream(decodedFile);
+				final int DECODED_SOURCE_CHUNK = 300;
+				byte[] inputArray = new byte[DECODED_SOURCE_CHUNK];
+				boolean stop = false;
+				while(!stop) {
+					int bytesRead = inputStream.read(inputArray);
+					if(bytesRead == -1) {
+						stop = true;
+						continue;
+					} else {
+						if(bytesRead < DECODED_SOURCE_CHUNK) {
+							encodedData.append(encoder.encode(Arrays.copyOf(inputArray, bytesRead)));
+						} else
+							encodedData.append(encoder.encode(inputArray));
+							
+						if(bytesRead < inputArray.length) {
+							stop = true;
+						}
+					}
+				}
+				
+				return encodedData.toString();
+			} catch (FileNotFoundException fnfException) {
+				// TODO Auto-generated catch block
+				fnfException.printStackTrace();
+			}
+			catch (IOException ioException) {
+				// TODO Auto-generated catch block
+				ioException.printStackTrace();
+			}
+
+		}
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void encodeAndSaveAs(File selectedFile) {
+		String encodedData = encodeData();
+		if(encodedData == null)
+			JOptionPane.showMessageDialog(this, "There is an error occurred during data encoding", "Encoding error", JOptionPane.ERROR_MESSAGE);
+		else {
+			OutputStream outputStream;
+			try {
+				outputStream = new FileOutputStream(selectedFile);
+				outputStream.write(encodedData.getBytes());
 			} catch (FileNotFoundException fnfException) {
 				// TODO Auto-generated catch block
 				fnfException.printStackTrace();
